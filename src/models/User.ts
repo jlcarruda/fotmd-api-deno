f0452e5e4cdc3ec8a2ca1a11e0b60ea1a97d652c
Joao Lucas <jlcarruda3@gmail.com>
2020-05-21 17:11:07 -0300

    implemented schema on User model; added signup method to User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,27 @@
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
-import { UserAuthenticationError } from '../error-handler.ts'
+import { UserAuthenticationError, UsernameConflictError } from '../error-handler.ts'
 import Model from './dependencies/Model.ts';
+
+type SignupPayload = { username: string, rawPassword: string }
+type AuthenticatePayload = { username: string, password: string }
 
 export default class User extends Model {
   constructor() {
     super('users')
+    this.schema = {
+      username: { type: 'string' },
+      password: { type: 'string' },
+      characters: [],
+      tables_owned: [],
+      tables_participating: [],
+      created_at: { type: 'date', default: new Date() },
+      updated_at: { type: 'date', default: new Date() }
+    }
   }
 
-  public async authenticate({ username, password }: { username: string, password: string }): Promise<any> {
+  public async authenticate({ username, password }: AuthenticatePayload): Promise<any> {
     try {
-      const user = await this.getCollection()?.findOne({ username })
+      const user = await this.findByUsername(username)
       if (!user || !await bcrypt.compare(password, user.password)) {
         return Promise.reject(new UserAuthenticationError("Wrong Credentials"))
       }
@@ -18,14 +30,28 @@ export default class User extends Model {
       console.error(`Error - MODEL - User.authenticate: ${error}`)
       throw error
     }
-
   }
 
-  public async signup() {
-    //TODO
+  public async signup({ username, rawPassword }: SignupPayload) {
+    try {
+      const user = await this.findByUsername(username)
+      if (user) return Promise.reject(new UsernameConflictError("Username is not available"))
+      const password = await this.encryptPassword(rawPassword)
+
+      await this.insertOne({
+        username,
+        password
+      })
+    } catch(error) {
+
+    }
   }
 
   private async encryptPassword(password: string) {
-    // return await bcrypt.hash(password)
+    return bcrypt.hash(password)
+  }
+
+  private async findByUsername(username: string) {
+    return this.getCollection()?.findOne({ username })
   }
 }
