932c56602ae21486862dd5edb412fa0bde67cdc4
Joao Lucas <jlcarruda3@gmail.com>
2020-05-21 17:11:46 -0300

    added signup route to routes.ts; refactored README

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ export function createRouter (): Router {
   const router = new Router();
   router
     .post('/user/auth', ctx => UserRules.userAuth(ctx))
+    .post('/user/auth', ctx => UserRules.signup(ctx))
     .get('/user/profile', ctx => UserRules.getProfile(ctx))
     .get('/user/characters', ctx => UserRules.getCharacters(ctx))
 
